5625f8fe88108abf8dfa67e6e8fc663a94f333b8
Yelitza3125 <deyel_99@hotmail.com>
2018-02-11 17:36:14 -0500

    Redirección a la vista work

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 
-$( document ).ready(function() {
+$(document).ready(function() {
   $('.col-one').addClass('animated slideInLeft');
   $('.deya').addClass('animated slideInLeft');
   $('.col-two').addClass('animated slideInRight'),
@@ -7,10 +7,9 @@ $( document ).ready(function() {
   $('.display-flex li').addClass('animated slideInUp');
 
 
-  setInterval(function(){ 
+  setInterval(function() { 
     $('.col-one').addClass('animated slideOutLeft'); 
     $('.col-two').addClass('animated slideOutRight');
-    
   }, 2000);
 
   setInterval(function() {
@@ -19,43 +18,42 @@ $( document ).ready(function() {
   }, 2700);
 
 
-
   setInterval(function changeFormDarkBlue() {
-    var sizeChange = ['20%', '10%','25%', '30%'];
-    var heightChange = ['10vh', '20vh','25vh', '30vh'];
+    var sizeChange = ['20%', '10%', '25%', '30%'];
+    var heightChange = ['10vh', '20vh', '25vh', '30vh'];
    
     $('.blue-soft').animate(
-      {width: sizeChange[Math.floor(Math.random()*4)],
-      height: heightChange[Math.floor(Math.random()*4)]})
+      {width: sizeChange[Math.floor(Math.random() * 4)],
+        height: heightChange[Math.floor(Math.random() * 4)]});
     $('.blue-soft').toggleClass('pink-one');  
-    }, 2000);
+  }, 2000);
   
-    setInterval(function changeFormSoftBlue() {
-      var sizeChangeTwo = ['25%', '15%','28%', '18%'];
-      var heightChange = ['10vh', '18vh','30vh', '22vh'];
+  setInterval(function changeFormSoftBlue() {
+    var sizeChangeTwo = ['25%', '15%', '28%', '18%'];
+    var heightChange = ['10vh', '18vh', '30vh', '22vh'];
      
-        $('.dark-blue').animate(
-          {width: sizeChangeTwo[Math.floor(Math.random()*4)],
-            height: heightChange[Math.floor(Math.random()*4)]})
-        $('.dark-blue').toggleClass('pink-soft');     
-      }, 3000);
+    $('.dark-blue').animate(
+      {width: sizeChangeTwo[Math.floor(Math.random() * 4)],
+        height: heightChange[Math.floor(Math.random() * 4)]});
+    $('.dark-blue').toggleClass('pink-soft');     
+  }, 3000);
   
   setInterval(function changeFormPinkDark() {
-    var sizeChangeThree = ['21%', '19%','23%', '30%'];
-    var heightChange = ['12vh', '22vh','30vh', '15vh'];
+    var sizeChangeThree = ['21%', '19%', '23%', '30%'];
+    var heightChange = ['12vh', '22vh', '30vh', '15vh'];
     $('.pink-one').animate(
-      {width: sizeChangeThree[Math.floor(Math.random()*4)],
-        height: heightChange[Math.floor(Math.random()*4)]}) 
-  $('.pink-one').toggleClass('dark-blue');  
+      {width: sizeChangeThree[Math.floor(Math.random() * 4)],
+        height: heightChange[Math.floor(Math.random() * 4)]}); 
+    $('.pink-one').toggleClass('dark-blue');  
   }, 2000);    
   
   setInterval(function changeFormPinkSoft() {
-    var sizeChangeFour = ['22%', '24%','15%', '31%'];
-    var heightChange = ['17vh', '20vh','25vh', '30vh'];
-  $('.soft-pink').animate(
-    {width: sizeChangeFour[Math.floor(Math.random()*4)],
-      height: heightChange[Math.floor(Math.random()*4)]})  
-  $('.soft-pink').toggleClass('blue-soft');   
+    var sizeChangeFour = ['22%', '24%', '15%', '31%'];
+    var heightChange = ['17vh', '20vh', '25vh', '30vh'];
+    $('.soft-pink').animate(
+      {width: sizeChangeFour[Math.floor(Math.random() * 4)],
+        height: heightChange[Math.floor(Math.random() * 4)]});  
+    $('.soft-pink').toggleClass('blue-soft');   
   }, 3000);
 
   /* Funcionalidad del navbar */
@@ -64,28 +62,15 @@ $( document ).ready(function() {
     $('.menu-icon').removeClass('paused').addClass('active');
     $('.menu-icon').addClass('hide');
     $('.menu').removeClass('paused').addClass('active');
-});
+  });
 
-$('.menu').bind('click', function() {
+  $('.menu').bind('click', function() {
     $('.menu-icon').removeClass('active');
     $('.menu').removeClass('active');
+  });
+
+  /* Redireccionando a la siguiente vista*/
+  $('.work').click(function() {
+    window.location.href = 'views/work.html'; 
+  });
 });
-
-/*
-setInterval(function() {
-  $('.figure-container').toggleClass('style-figure');
-},10000); */
-});
-
-
-
-
-/*
-$('#animar-2').on('click', function() {
-    $('#animable')
-      .animate({left: '50%'}, 'slow')
-      .animate({left: '40%'}, 'slow')
-      .animate({width: 200}, 'fast')
-      .animate({'font-size': '3rem'}, 1000);
-  })
-}*/
