a38118bdbc0476ed50297f4a8640a7043128aeff
Yelitza3125 <deyel_99@hotmail.com>
2018-02-16 13:26:36 -0500

    Vista Contact

--- js/app.js
+++ js/app.js
@@ -20,7 +20,7 @@ $(document).ready(function() {
 
   setInterval(function changeFormDarkBlue() {
     var sizeChange = ['20%', '10%', '25%', '30%'];
-    var heightChange = ['10vh', '20vh', '25vh', '30vh'];
+    var heightChange = ['10vh', '20vh', '25vh', '18vh'];
    
     $('.blue-soft').animate(
       {width: sizeChange[Math.floor(Math.random() * 4)],
@@ -30,31 +30,31 @@ $(document).ready(function() {
   
   setInterval(function changeFormSoftBlue() {
     var sizeChangeTwo = ['25%', '15%', '28%', '18%'];
-    var heightChange = ['10vh', '18vh', '30vh', '22vh'];
+    var heightChange = ['10vh', '18vh', '15vh', '22vh'];
      
     $('.dark-blue').animate(
       {width: sizeChangeTwo[Math.floor(Math.random() * 4)],
         height: heightChange[Math.floor(Math.random() * 4)]});
     $('.dark-blue').toggleClass('pink-soft');     
-  }, 4000);
+  }, 5000);
   
   setInterval(function changeFormPinkDark() {
     var sizeChangeThree = ['21%', '19%', '23%', '30%'];
-    var heightChange = ['12vh', '22vh', '30vh', '15vh'];
+    var heightChange = ['12vh', '22vh', '18vh', '15vh'];
     $('.pink-one').animate(
       {width: sizeChangeThree[Math.floor(Math.random() * 4)],
         height: heightChange[Math.floor(Math.random() * 4)]}); 
     $('.pink-one').toggleClass('dark-blue');  
-  }, 3000);    
+  }, 9000);    
   
   setInterval(function changeFormPinkSoft() {
     var sizeChangeFour = ['22%', '24%', '15%', '31%'];
-    var heightChange = ['17vh', '20vh', '25vh', '30vh'];
+    var heightChange = ['17vh', '20vh', '25vh', '10vh'];
     $('.soft-pink').animate(
       {width: sizeChangeFour[Math.floor(Math.random() * 4)],
         height: heightChange[Math.floor(Math.random() * 4)]});  
     $('.soft-pink').toggleClass('blue-soft');   
-  }, 4000);
+  }, 12000);
   
 
   /* Funcionalidad del navbar */
