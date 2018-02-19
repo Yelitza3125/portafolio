4c1627fbb21fb77db31b1440975ba927137f1e2e
Yelitza3125 <deyel_99@hotmail.com>
2018-02-19 01:57:18 -0500

    Vista contact

---
+++ js/contact.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  /* Código del carrusel 
+  setInterval(function() {
+    $('.one, .six').addClass('animated fadeIn');  
+  }, 5000);
+
+  setInterval(function() {
+    $('.three, .five').addClass('animated fadeIn');  
+  }, 18000);
+
+  setInterval(function() {
+    $('.four, .eight').addClass('animated fadeIn');  
+  }, 12000);
+
+  setInterval(function() {
+    $('.two, .seven').addClass('animated fadeIn');  
+  }, 20000); */
+});
